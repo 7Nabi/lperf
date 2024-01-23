426eb65f884fb642e3da7de1b5c543c9dad6dba9
7Nabi <134373429+7Nabi@users.noreply.github.com>
2024-01-23 12:41:38 +0400

    Create item_page11.dart

---
+++ item_page11.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:lperf/cart_page.dart';
+
+class ItemPage11 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEDECF2),
+      appBar: AppBar(
+        title: Text('Product'),
+        backgroundColor: Color(0xff0a126b),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Image.network(
+                'https://pimcdn.sharafdg.com/images/000000000001083259_2?1703596745',
+                height: 300),
+          ),
+          Row(
+            children: [
+              Text(
+                " BLUE",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Color(0xff0a126b),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 2),
+                child: Text(
+                  " Brand:",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff0a126b),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            child: Text(
+              " Chanel",
+              style: TextStyle(
+                fontSize: 17,
+                color: Color(0xff0a126b),
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              " The components:",
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff0a126b),
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              " cedar, and sandalwood",
+              style: TextStyle(
+                fontSize: 17,
+                color: Color(0xff0a126b),
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              " Bottle Size:",
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff0a126b),
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              " 100 ml",
+              style: TextStyle(
+                fontSize: 17,
+                color: Color(0xff0a126b),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 3),
+                child: Text(
+                  "  71 OMR",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            child: Stack(children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CartPage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 35,
+                  width: 100,
+                  margin: EdgeInsets.only(left: 125),
+                  decoration: BoxDecoration(
+                      color: Color(0xff0a126b),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Center(
+                    child: Text(
+                      'Add to cart',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
