2d5b381197c0d69e16aedc6e6e072bc2572237c2
7Nabi <134373429+7Nabi@users.noreply.github.com>
2024-01-23 12:42:12 +0400

    Create item_page12.dart

---
+++ item_page12.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:lperf/cart_page.dart';
+
+class ItemPage12 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEDECF2),
+      appBar: AppBar(
+        title: Text('Product'),
+        backgroundColor: Color(0xff0a126b),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Image.network(
+                'https://www.chanel.com/images/w_0.51,h_0.51,c_crop/q_auto:good,f_auto,fl_lossy,dpr_1.1/w_1920/n-19-eau-de-parfum-spray-3-4fl-oz--packshot-default-119530-9519205285918.jpg',
+                height: 300),
+          ),
+          Row(
+            children: [
+              Text(
+                " N°19",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Color(0xff0a126b),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 2),
+                child: Text(
+                  " Brand:",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff0a126b),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            child: Text(
+              " Chanel",
+              style: TextStyle(
+                fontSize: 17,
+                color: Color(0xff0a126b),
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              " The components:",
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff0a126b),
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              " galbanum, May rose, iris, neroli",
+              style: TextStyle(
+                fontSize: 17,
+                color: Color(0xff0a126b),
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              " Bottle Size:",
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff0a126b),
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              " 14.8 ml",
+              style: TextStyle(
+                fontSize: 17,
+                color: Color(0xff0a126b),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 3),
+                child: Text(
+                  "  107 OMR",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            child: Stack(children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CartPage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 35,
+                  width: 100,
+                  margin: EdgeInsets.only(left: 125),
+                  decoration: BoxDecoration(
+                      color: Color(0xff0a126b),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Center(
+                    child: Text(
+                      'Add to cart',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
